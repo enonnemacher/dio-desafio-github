0b9d8fa684ab3873ea7ef4410af671dc241f605f
enonnemacher <enonnemacher@gmail.com>
2022-03-05 08:45:54 -0300

    Aplicando encapsulamento

--- desafio-banco-java-oo/src/com/enonnemacher/entities/Cliente.java
+++ desafio-banco-java-oo/src/com/enonnemacher/entities/Cliente.java
@@ -2,5 +2,13 @@ package com.enonnemacher.entities;
 
 public class Cliente {
 
-    public String nome;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
