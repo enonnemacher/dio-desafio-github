016cda1320d84f8652ba7439319fb95a27ece582
enonnemacher <enonnemacher@gmail.com>
2022-03-08 21:22:47 -0300

    Finalizado projeto

--- desafio-dio-POO/src/br/com/dio/desafio/application/Main.java
+++ desafio-dio-POO/src/br/com/dio/desafio/application/Main.java
@@ -1,7 +1,8 @@
 package br.com.dio.desafio.application;
 
-import br.com.dio.desafio.domain.Conteudo;
+import br.com.dio.desafio.domain.Bootcamp;
 import br.com.dio.desafio.domain.Curso;
+import br.com.dio.desafio.domain.Dev;
 import br.com.dio.desafio.domain.Mentoria;
 
 import java.time.LocalDate;
@@ -23,5 +24,27 @@ public class Main {
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descricao de mentoria Java");
         mentoria.setData(LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(curso2);
+
+        Dev developerEderson = new Dev();
+        developerEderson.setNome("Ederson");
+        developerEderson.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + developerEderson.getConteudosInscritos());
+        developerEderson.progredir();
+        System.out.println("Conteúdos Concluídos" + developerEderson.getConteudosConcluidos());
+        System.out.println("XP: " + developerEderson.calcularXp());
+
+        Dev developerJoao = new Dev();
+        developerJoao.setNome("João");
+        developerJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + developerJoao.getConteudosInscritos());
+        developerJoao.progredir();
+        System.out.println("Conteúdos Concluídos" + developerJoao.getConteudosConcluidos());
+        System.out.println("XP: " + developerJoao.calcularXp());
     }
 }
